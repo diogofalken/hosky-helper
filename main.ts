f99a42e2aebad9665e3bbbc9b31dde8d122e64d0
diogofalken <diogofalken0@gmail.com>
2021-11-23 16:31:10 +0000

    feat: Added table view and new fields
    - Now it's possible to show ada balance using `--show-ada` flag
    
    - Table is being shown with cliffy
    
    - Using command from cliffy it is possible to interact better with CLI tool

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+import { Command } from 'https://deno.land/x/cliffy@v0.20.1/command/command.ts';
+import { ICell } from 'https://deno.land/x/cliffy@v0.20.1/table/cell.ts';
+import { IRow } from 'https://deno.land/x/cliffy@v0.20.1/table/row.ts';
+import { Table } from 'https://deno.land/x/cliffy@v0.20.1/table/table.ts';
 import { fetchWallet } from 'https://raw.githubusercontent.com/diogofalken/wallet-nft-calculator/fb9636f7075706958cb8ef75e0adec4309e17c72/lib/main.ts';
 import { wallets } from './wallets.ts';
 
@@ -5,8 +9,8 @@ function convertToBillion(value: number) {
   return (value / 1000000000).toFixed(2);
 }
 
-function getHoskyValue(value: number) {
-  return `${value} | ${convertToBillion(value)}B`;
+function getHoskyValuePerB(value: number) {
+  return `${convertToBillion(value)}B`;
 }
 
 function convertADA(price: number) {
@@ -17,10 +21,11 @@ function convertADA(price: number) {
   return Math.round(price / 1000000);
 }
 
-async function main() {
+export async function generateReport(showAda: boolean) {
+  const report = [];
   let totalHosky = 0;
+  let totalAda = 0;
 
-  console.log('----------');
   for (const [i, addr] of wallets.entries()) {
     const { tokens, lovelaces } = await fetchWallet(addr);
 
@@ -31,15 +36,56 @@ async function main() {
     if (curHosky === undefined) continue;
 
     totalHosky += curHosky.quantity;
+    totalAda += convertADA(lovelaces);
 
-    console.log(
-      `Wallet ${i + 1} (${convertADA(lovelaces)}A): ${getHoskyValue(
-        curHosky.quantity,
-      )}`,
-    );
+    report.push([
+      `#${i + 1}`,
+      showAda ? `${convertADA(lovelaces)}A` : '###',
+      curHosky.quantity,
+      getHoskyValuePerB(curHosky.quantity),
+    ]);
   }
-  console.log('----------');
-  console.log(`Total: ${getHoskyValue(totalHosky)}`);
+
+  report.push([
+    'Total:',
+    `${totalAda}A`,
+    totalHosky,
+    getHoskyValuePerB(totalHosky),
+  ]);
+  return report;
+}
+
+function renderTableReport(body: IRow<ICell>[]) {
+  new Table()
+    .header(['Wallet', 'ADA', '$HOSKY', '$HOSKY/B'])
+    .body(body)
+    .maxColWidth(13)
+    .padding(1)
+    .border(true)
+    .render();
+}
+
+async function main() {
+  const { options } = await new Command()
+    .name('hosky-helper')
+    .description(
+      'Get your total hoskys in your wallets.\nYou have to define all your wallet addresses in `wallet.ts` file and you are good to go.',
+    )
+    .version('0.0.1')
+    .option(
+      '-s, --show-ada [show-ada:boolean]',
+      'Show the ADA in your wallets?',
+      {
+        required: true,
+        default: false,
+      },
+    )
+    .parse(Deno.args);
+
+  const { showAda } = options;
+
+  const report = await generateReport(showAda);
+  renderTableReport(report);
 }
 
 if (import.meta.main === true) {
